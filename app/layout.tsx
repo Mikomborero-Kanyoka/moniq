500ed6acd217a348e3a42bc447328b681f28d481
Mikomborero-Kanyoka <aiqkanyoka@gmail.com>
2026-04-27 16:21:30 +0200

    "update-icon"

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,21 +20,8 @@ export const metadata: Metadata = {
   generator: "Next.js",
   category: "technology",
   icons: {
-    icon: [
-      {
-        url: "/icon-light-32x32.png",
-        media: "(prefers-color-scheme: light)",
-      },
-      {
-        url: "/icon-dark-32x32.png",
-        media: "(prefers-color-scheme: dark)",
-      },
-      {
-        url: "/icon.svg",
-        type: "image/svg+xml",
-      },
-    ],
-    apple: "/apple-icon.png",
+    icon: "/images/logo.png",
+    apple: "/images/logo.png",
   },
   robots: {
     index: true,
